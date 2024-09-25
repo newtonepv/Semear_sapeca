15b267ee4d1faddd46beecfc19a973aba362ee55
Newton Eduardo Pena Villegas <newtonepv@gmail.com>
2024-09-25 17:19:59 -0300

    updated detector to be detector_certo

--- detector.py
+++ detector.py
@@ -1,185 +1,136 @@
-import cv2 as cv
+import cv2
 import numpy as np
-import cv2 as cv
 
-def get_limits(color):
-    """
-    Ajusta os limites HSV para a cor fornecida com base na imagem capturada.
-    """
-    c = np.uint8([[color]])
-    hsvC = cv.cvtColor(c, cv.COLOR_BGR2HSV)
-    h, s, v = hsvC[0][0]
+cap = cv2.VideoCapture(0)
 
-    if color == (0, 255, 255):  # Amarelo
-        lower_limit = np.array([h - 20, 100, 100], dtype=np.uint8)
-        upper_limit = np.array([h + 20, 255, 255], dtype=np.uint8)
-    elif color == (0, 0, 255):  # Vermelho
-        lower_limit2 = np.array([160, 100, 100], dtype=np.uint8)
-        upper_limit2 = np.array([180, 255, 255], dtype=np.uint8)
-        return lower_limit2, upper_limit2
-    elif color == (0, 165, 255):  # Laranja
-        lower_limit = np.array([5, 100, 100], dtype=np.uint8)
-        upper_limit = np.array([15, 255, 255], dtype=np.uint8)
-    elif color == (0, 255, 0):  # Verde
-        lower_limit = np.array([35, 50, 50], dtype=np.uint8)
-        upper_limit = np.array([85, 255, 255], dtype=np.uint8)
-    elif color == (255, 0, 0):  # Azul
-        lower_limit = np.array([100, 100, 100], dtype=np.uint8)
-        upper_limit = np.array([140, 255, 255], dtype=np.uint8)
-    elif color == (255, 255, 255):  # Branco
-        lower_limit = np.array([0,0,0], dtype=np.uint8)
-        upper_limit = np.array([200,0,150], dtype=np.uint8)
+# Define color ranges for HSV
+lower_range_az = np.array([69, 5, 0])
+upper_range_az = np.array([156, 94, 77])
+
+lower_range_am = np.array([0, 86, 0])
+upper_range_am = np.array([28, 157, 175])
+
+lower_range_vrd = np.array([0, 56, 0])
+upper_range_vrd = np.array([112, 129, 50])
+
+lower_range_vrm = np.array([26, 0, 101])
+upper_range_vrm = np.array([255, 37, 255])
+
+lower_range_br = np.array([81, 58, 17])
+upper_range_br = np.array([103, 176, 149])
+
+lower_range_lar = np.array([0, 0, 0])
+upper_range_lar = np.array([15, 116, 255])
+
+# Função para equalizar a imagem
+def hisEqulColor(img):
+    ycrcb = cv2.cvtColor(img, cv2.COLOR_BGR2YCR_CB)
+    channels = cv2.split(ycrcb)
+    cv2.equalizeHist(channels[0], channels[0])
+    cv2.merge(channels, ycrcb)
+    return img
+
+# Função para aplicar erode e dilate
+def process_mask(mask):
+    kernel = np.ones((5, 5), np.uint8)
+    mask = cv2.erode(mask, kernel, iterations=1)
+    mask = cv2.dilate(mask, kernel, iterations=1)
+    return mask
+
+# Funções para cada cor com erode e dilate
+def azul(frame):
+    mask = cv2.inRange(frame, lower_range_az, upper_range_az)
+    mask = process_mask(mask)
+    _, mask1 = cv2.threshold(mask, 254, 255, cv2.THRESH_BINARY)
+    cnts, _ = cv2.findContours(mask1, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    a = []
+    for c in cnts:
+        if cv2.contourArea(c) > 600:
+            x, y, w, h = cv2.boundingRect(c)
+            a.append(x)
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 1)
+    cv2.putText(frame, ("AZUL " + str(len(a))), (10, 360), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 0, 0), 2)
+
+def verde(frame):
+    mask = cv2.inRange(frame, lower_range_vrd, upper_range_vrd)
+    mask = process_mask(mask)
+    _, mask1 = cv2.threshold(mask, 254, 255, cv2.THRESH_BINARY)
+    cnts, _ = cv2.findContours(mask1, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    a = []
+    for c in cnts:
+        if cv2.contourArea(c) > 600:
+            x, y, w, h = cv2.boundingRect(c)
+            a.append(x)
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 1)
+    cv2.putText(frame, ("VERDE " + str(len(a))), (10, 380), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
+
+def vermelho(frame):
+    mask = cv2.inRange(frame, lower_range_vrm, upper_range_vrm)
+    mask = process_mask(mask)
+    _, mask1 = cv2.threshold(mask, 254, 255, cv2.THRESH_BINARY)
+    cnts, _ = cv2.findContours(mask1, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    a = []
+    for c in cnts:
+        if cv2.contourArea(c) > 600:
+            x, y, w, h = cv2.boundingRect(c)
+            a.append(x)
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 1)
+    cv2.putText(frame, ("VERMELHO " + str(len(a))), (10, 400), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
+
+def amarelo(frame):
+    mask = cv2.inRange(frame, lower_range_am, upper_range_am)
+    mask = process_mask(mask)
+    _, mask1 = cv2.threshold(mask, 254, 255, cv2.THRESH_BINARY)
+    cnts, _ = cv2.findContours(mask1, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    a = []
+    for c in cnts:
+        if cv2.contourArea(c) > 600:
+            x, y, w, h = cv2.boundingRect(c)
+            a.append(x)
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 255), 2)
+    cv2.putText(frame, ("AMARELO " + str(len(a))), (10, 420), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 255), 2)
+
+def branco(frame):
+    mask = cv2.inRange(frame, lower_range_br, upper_range_br)
+    mask = process_mask(mask)
+    _, mask1 = cv2.threshold(mask, 254, 255, cv2.THRESH_BINARY)
+    cnts, _ = cv2.findContours(mask1, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    a = []
+    for c in cnts:
+        if cv2.contourArea(c) > 600:
+            x, y, w, h = cv2.boundingRect(c)
+            a.append(x)
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 255, 255), 2)
+    cv2.putText(frame, ("BRANCO " + str(len(a))), (10, 440), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2)
+
+def laranja(frame):
+    mask = cv2.inRange(frame, lower_range_lar, upper_range_lar)
+    mask = process_mask(mask)
+    _, mask1 = cv2.threshold(mask, 254, 255, cv2.THRESH_BINARY)
+    cnts, _ = cv2.findContours(mask1, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    a = []
+    for c in cnts:
+        if cv2.contourArea(c) > 600:
+            x, y, w, h = cv2.boundingRect(c)
+            a.append(x)
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 128, 255), 2)
+    cv2.putText(frame, ("LARANJA " + str(len(a))), (10, 460), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 128, 255), 2)
+
+while True:
+    ret, frame = cap.read()
+    frame = cv2.resize(frame, (640, 480))
+    frame = hisEqulColor(frame)
     
-    return lower_limit, upper_limit
-
-
-def get_limits(color):
-    """
-    Ajusta os limites HSV para a cor fornecida com base na imagem capturada.
-    """
-    c = np.uint8([[color]])
-    hsvC = cv.cvtColor(c, cv.COLOR_BGR2HSV)
-    h, s, v = hsvC[0][0]
-
-    if color == (0, 255, 255):  # Amarelo
-        lower_limit = np.array([h - 20, 100, 100], dtype=np.uint8)
-        upper_limit = np.array([h + 20, 255, 255], dtype=np.uint8)
-    elif color == (0, 0, 255):  # Vermelho
-        lower_limit2 = np.array([160, 100, 100], dtype=np.uint8)
-        upper_limit2 = np.array([180, 255, 255], dtype=np.uint8)
-        return lower_limit2, upper_limit2
-    elif color == (0, 165, 255):  # Laranja
-        lower_limit = np.array([5, 100, 100], dtype=np.uint8)
-        upper_limit = np.array([15, 255, 255], dtype=np.uint8)
-    elif color == (0, 255, 0):  # Verde
-        lower_limit = np.array([35, 50, 50], dtype=np.uint8)
-        upper_limit = np.array([85, 255, 255], dtype=np.uint8)
-    elif color == (255, 0, 0):  # Azul
-        lower_limit = np.array([100, 100, 100], dtype=np.uint8)
-        upper_limit = np.array([140, 255, 255], dtype=np.uint8)
-    elif color == (255, 255, 255):  # Branco
-        lower_limit = np.array([0,0,0], dtype=np.uint8)
-        upper_limit = np.array([200,0,150], dtype=np.uint8)
+    azul(frame)
+    amarelo(frame)
+    verde(frame)
+    vermelho(frame)
+    laranja(frame)
+    branco(frame)
     
-    return lower_limit, upper_limit
+    cv2.imshow("FRAME", frame)
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
 
-def process_color(color_name, color, hsv, frame):
-    """
-    Processa a detecção de uma cor específica e desenha as caixas delimitadoras.
-    """
-    lower_bound, upper_bound = get_limits(color)
-    mask = cv.inRange(hsv, lower_bound, upper_bound)
-
-    
-    result = cv.bitwise_and(frame, frame, mask=mask)
-    gray = cv.cvtColor(result, cv.COLOR_BGR2GRAY)
-    edges = cv.Canny(gray, 100, 200)
-    contours, _ = cv.findContours(edges, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
-    
-    for contour in contours:
-        if cv.contourArea(contour) > 200:
-            x, y, w, h = cv.boundingRect(contour)
-            cv.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 1)
-            cv.circle(frame, (x + w // 2, y + h // 2), 3, (0, 255, 0), -1)
-            cv.putText(frame, color_name, (x, y - 10), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-    
-    return result
-
-def main():
-    # Inicializa a captura de vídeo
-    cap = cv.VideoCapture(0)
-    if not cap.isOpened():
-        print("Erro ao abrir a câmera.")
-        return
-    
-    colors = {
-        'white': (255, 255, 255),
-        'yellow': (0, 255, 255),
-        'red': (0, 0, 255),
-        'orange': (0, 165, 255),
-        'green': (0, 255, 0),
-        'blue': (255, 0, 0)
-    }
-    
-    while True:
-        ret, frame = cap.read()
-        if not ret:
-            print("Erro ao capturar imagem.")
-            break
-        
-        # Normalização do histograma para reduzir o impacto da iluminação
-        lab = cv.cvtColor(frame, cv.COLOR_BGR2LAB)
-        l, a, b = cv.split(lab)
-        l = cv.equalizeHist(l)
-        lab = cv.merge((l, a, b))
-        frame = cv.cvtColor(lab, cv.COLOR_LAB2BGR)
-
-        
-        hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
-        
-        combined_result = np.zeros_like(frame)
-        
-        for color_name, color in colors.items():
-            result = process_color(color_name, color, hsv, frame)
-            combined_result = cv.add(combined_result, result)
-        combined_result = cv.GaussianBlur(combined_result, (3, 3), 1)
-        cv.imshow('Combined Result', combined_result)
-
-        if cv.waitKey(1) & 0xFF == ord('q'):
-            break
-    
-    cap.release()
-    cv.destroyAllWindows()
-
-if __name__ == "__main__":
-    main()
-
-
-def main():
-    # Inicializa a captura de vídeo
-    cap = cv.VideoCapture(0)
-    if not cap.isOpened():
-        print("Erro ao abrir a câmera.")
-        return
-    
-    colors = {
-        'white': (255, 255, 255),
-        'yellow': (0, 255, 255),
-        'red': (0, 0, 255),
-        'orange': (0, 165, 255),
-        'green': (0, 255, 0),
-        'blue': (255, 0, 0)
-    }
-    
-    while True:
-        ret, frame = cap.read()
-        if not ret:
-            print("Erro ao capturar imagem.")
-            break
-        
-        # Normalização do histograma para reduzir o impacto da iluminação
-        lab = cv.cvtColor(frame, cv.COLOR_BGR2LAB)
-        l, a, b = cv.split(lab)
-        l = cv.equalizeHist(l)
-        lab = cv.merge((l, a, b))
-        frame = cv.cvtColor(lab, cv.COLOR_LAB2BGR)
-
-        
-        hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
-        
-        combined_result = np.zeros_like(frame)
-        
-        for color_name, color in colors.items():
-            result = process_color(color_name, color, hsv, frame)
-            combined_result = cv.add(combined_result, result)
-        combined_result = cv.GaussianBlur(combined_result, (21, 21), 10)
-        cv.imshow('Combined Result', combined_result)
-
-        if cv.waitKey(1) & 0xFF == ord('q'):
-            break
-    
-    cap.release()
-    cv.destroyAllWindows()
-
-if __name__ == "__main__":
-    main()
+cap.release()
+cv2.destroyAllWindows()
